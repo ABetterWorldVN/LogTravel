70a97ef854dd6c7261561ee01e42171e5dac3798
vinh.nguyen <vinh.nguyen@SGCP-TMA030>
2020-02-27 21:41:32 +0700

    Create Mongoose Connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParse = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const user = require('./routes/user');
 
 const PORT = process.env.PORT || 3009;
+
+// Connect DB
+mongoose.connect('mongodb://threesometeam:aaa123@ds149030.mlab.com:49030/log-travel', {useNewUrlParser: true, useUnifiedTopology: true});
+let db = mongoose.connection;
+db.on('Connect', console.log('Is connected'));
 
 app.use(express.json());
 
