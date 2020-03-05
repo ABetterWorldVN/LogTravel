fdf21d40b0c9597b39ebb009210aaadb08f82429
quantran <quan@zoolooinfosys.com>
2020-03-05 23:29:37 +0800

    * fix awesome bug

--- app/src/components/LogTravelItem.vue
+++ app/src/components/LogTravelItem.vue
@@ -1,14 +1,26 @@
 <template>
-  <li> {{ place.name }}</li>
+  <div>
+    <li>
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="" alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">{{place.name}}</h5>
+          <p class="card-text">{{place.description}}</p>
+          <p class="card-text">{{place.comment}}</p>
+          <a href="#" class="btn btn-primary">Delete</a>
+        </div>
+      </div>
+    </li>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "LogTravelItem",
-  props: {
-    place: {}
-  }
-};
+    export default {
+    name: "LogTravelItem",
+    props: {
+        place: {}
+    }
+    };
 </script>
 
 <style scoped>
